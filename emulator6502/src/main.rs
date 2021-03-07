fed76fb4a885186a261769ada2791082d314fcfd
Mike Simms <msimms01@gmail.com>
2021-03-07 16:22:59 -0500

    Added cpu struct.

--- emulator6502/src/main.rs
+++ emulator6502/src/main.rs
@@ -1,3 +1,26 @@
+struct Cpu {
+    pc: u16, // program counter
+    sp: u16, // stack pointer
+    a: u8, // a reg
+    x: u8, // x reg
+    y: u8, // y reg
+    status: u8, // status flags
+}
+
+fn reset_cpu(cpu: &mut Cpu) {
+    cpu.pc = 0xfffc;
+    cpu.sp = 0x0100;
+    cpu.a = 0;
+    cpu.x = 0;
+    cpu.y = 0;
+    cpu.status = 0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = Cpu{pc:0, sp:0, a:0, x:0, y:0, status:0};
+
+    println!("Initializing");
+    reset_cpu(&mut cpu);
+
+    println!("Done");
 }
