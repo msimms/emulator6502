fa4ce6d8d823e6274694a9de190c1d079be68086
Mike Simms <msimms01@gmail.com>
2021-03-07 17:47:47 -0500

    Added memory.

--- emulator6502/src/main.rs
+++ emulator6502/src/main.rs
@@ -1,3 +1,9 @@
+const MEM_SIZE: usize = 1024 * 64;
+
+struct Mem {
+    data: [u8; MEM_SIZE],
+}
+
 struct Cpu {
     pc: u16, // program counter
     sp: u16, // stack pointer
@@ -7,20 +13,22 @@ struct Cpu {
     status: u8, // status flags
 }
 
-fn reset_cpu(cpu: &mut Cpu) {
+fn reset(cpu: &mut Cpu, mem: &mut Mem) {
     cpu.pc = 0xfffc;
     cpu.sp = 0x0100;
     cpu.a = 0;
     cpu.x = 0;
     cpu.y = 0;
     cpu.status = 0;
+    mem.data = [0; MEM_SIZE];
 }
 
 fn main() {
     let mut cpu = Cpu{pc:0, sp:0, a:0, x:0, y:0, status:0};
+    let mut mem = Mem{data:[0; MEM_SIZE]};
 
     println!("Initializing");
-    reset_cpu(&mut cpu);
+    reset(&mut cpu, &mut mem);
 
     println!("Done");
 }
